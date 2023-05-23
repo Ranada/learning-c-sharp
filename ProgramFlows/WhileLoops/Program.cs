5559ce3f679b70bcddd32e0222399ca79ede5fb8
Neil Ranada <neil.ranada@gmail.com>
2023-05-22 21:27:34 -0700

    While loop exercise complete

---
+++ ProgramFlows/WhileLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhileLoops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // While loop checks condition first before running
+        string? input = "";
+        // while(input != "exit")
+        // {
+        //     Console.WriteLine("Enter a command:");
+        //     input = Console.ReadLine();
+        //     Console.WriteLine("You entered: {0}\n", input);
+        // }
+
+        // Do-while loop runs at least one time
+        do
+        {
+            Console.WriteLine("Enter a command:");
+            input = Console.ReadLine();
+            Console.WriteLine("You entered: {0}\n", input);
+        }
+        while(input != "exit");
+    }
+}
