5ab40c3fbd0a92a55f75f73435f645457e1dd234
Neil Ranada <neil.ranada@gmail.com>
2023-05-23 14:22:27 -0700

    Initial commit for string operations exercise

---
+++ StringOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace StringOperations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string outstr;
+        string str1 = "The quick brown fox jumps over the lzy dog.";
+        string str2 = "This is a string";
+        string str1 = "THIS is a STRING";
+        string[] strs = {"one", "two", "three", "four"};
+        
+        // TODO: Length of a string
+
+        // TODO: Access individual characters
+
+        // TODO: Iterate over a string
+
+        // TODO: String Concat method
+
+        // TODO: String Join method
+
+        // TODO: String comparison
+            // Compare will perform an ordinal comparison and return:
+            // < 0 if first string comes before second string
+            // 0 if first string equals second string
+            // > 0 if first string comes after the second in sort ordr
+        
+        // TODO: Compare strings using Equals method
+
+        // TODO: Search string using IndexOf method
+
+        // TODO: Replace string using Replace method
+    }
+}
