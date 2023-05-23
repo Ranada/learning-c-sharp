430837a7a4e65ec13d6ecf9a22c08f91ef19a913
Neil Ranada <neil.ranada@gmail.com>
2023-05-22 17:12:07 -0700

    Practice making xml, multi-line, single-line comments

---
+++ Comments/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Comments;
+
+class Program
+{
+    ///This is an example of XML comment documentation
+    /// They start with triple-slashses and have a special syntax
+    /// <summary>
+    /// This is an example summary element
+    /// </summary>
+    /// <param name='args'>An array of string argument from the command line</param>
+    /// <returns>
+    /// No return value
+    /// </returns>
+    static void Main(string[] args)
+    {
+        // Single line comment
+        
+        Console.WriteLine("Hello, World!");
+
+        /*
+        Multi- line comments go here
+        Another line
+        Another line
+        Another line
+        Another line
+        */
+    }
+}
