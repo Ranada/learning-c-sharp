dd52274295cd497cb1650b7695e7ea3f03f1e2c4
Neil Ranada <neil.ranada@gmail.com>
2023-05-22 21:47:27 -0700

    Exceptions exercise complete

---
+++ ProgramFlows/Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exceptions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 10001;
+        int num2 = 10;
+        int result;
+
+        // Try, Catch, Finally statments
+        try 
+        {
+            if (num1 > 1000)
+            {
+                throw new ArgumentOutOfRangeException("num1", "num1 is greater than 1000");
+            }
+            result = num1 / num2;
+            Console.WriteLine("Result: {0}", result);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("Whoops!");
+            Console.WriteLine(e.Message);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.WriteLine("Oh snap!");
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Note: this code is in the `finally` block which always runs");
+        }
+    } 
+}
