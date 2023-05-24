98d755be9e1f794478fd7c204ff687d7876a50f3
Neil Ranada <neil.ranada@gmail.com>
2023-05-23 19:54:01 -0700

    Initial commit for StringBuilder exercise

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace StringBuilder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int jumpCount = 99;
+        string[] animals = {"wombat", "unicorn", "lemur"};
+
+        // TODO: create a StringBuilder
+
+        // TODO: print some basic stats about the StringBuilder
+
+        // TODO: Add some string to the builder using Append
+
+        // TODO: AppendLine can append a line ending
+
+        // TODO: AppendFormat can be used to append formatted strings
+
+        // TODO: AppendJoin can iterate over a set of values
+
+        // TODO: Modify the content using Replace
+
+        // TODO: Insert content at any index
+
+        // TODO: Convert to a single string
+    }
+}
