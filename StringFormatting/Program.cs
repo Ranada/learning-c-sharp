203a47366b18d5a7ed682a9927c181f6b194287f
Neil Ranada <neil.ranada@gmail.com>
2023-05-23 19:34:53 -0700

    String formatting exercise complete

--- StringFormatting/Program.cs
+++ StringFormatting/Program.cs
@@ -14,5 +14,35 @@ class Program
         // Basic formatting information
         Console.WriteLine("{0}", str1);
 
+        // TODO: Specifying numerical formatting
+        // General format is {index[,aligntment]:[format]}
+        // Common types are N (number), G (General), F (Fixed-point)
+        // E (Exponential, D (Decimal), P (Percent), X (Hexadecimal),
+        // C (Currency in local format)
+        Console.WriteLine("Number: {0:N}, General: {0:G}, Fixed point: {0:F}, Exponential: {0:E}", val1);
+        Console.WriteLine("Decimal: {0:E}, Number: {0:N}, Fixed point: {0:F}, General: {0:G}", val2);
+
+        // TODO: Add a number after the format to specify precision
+        Console.WriteLine("Decimal: {0:E3}, Number: {0:N7}, Fixed point: {0:F1}, General: {0:G3}", val2);
+
+        // TODO: Formatting with alignment and spacing
+        Console.WriteLine("\n-------Sales Data------");
+        foreach (int q in quarter) {
+            Console.Write("{0,12}", q);
+        }
+        Console.WriteLine();
+        
+        foreach (int sale in sales) {
+            Console.Write("{0,12:C2}", sale);
+        }
+        Console.WriteLine();
+        
+        foreach (double pct in intlMixPct) {
+            Console.Write("{0,12:P1}", pct);
+        }
+        Console.WriteLine();
+
+        // 
+
     }
 }
