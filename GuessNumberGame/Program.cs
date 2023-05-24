8f0a4cd567e1069acba76352b2b22599d6de04d3
Neil Ranada <neil.ranada@gmail.com>
2023-05-23 22:48:16 -0700

    Reduce number of if-statements

--- GuessNumberGame/Program.cs
+++ GuessNumberGame/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("\n----------------------Guess the Number Game-----------------------");
+        Console.WriteLine("\n--------------------------------Guess the Number Game--------------------------------");
         Console.WriteLine("I'm thinking of a number between 0 and 10.");
         Console.WriteLine("Enter your guess or 'exit' to leave the game.\n");
 
@@ -16,7 +16,7 @@ class Program
 
         while (continueGame)
         {
-            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------------------------------------");
             Console.WriteLine("What's your guess?");
             input = Console.ReadLine();
 
@@ -30,36 +30,20 @@ class Program
             bool isInteger = false;
             isInteger = Int32.TryParse(input, out guess);
 
-            if (isInteger)
+            if (isInteger && guess >= 0 && guess <= 20)
             {
                 Console.WriteLine($"Your guess: {guess}");
                 guessCount++;
 
-                if (guess < 0 || guess > 20)
-                {
-                    Console.WriteLine($"Whoops! Your guess is outside the range of 0 and 20.");
-                    Console.WriteLine($"Try again.");
-                    continue;
-                }
-
-                if (guess > randomNumber)
-                {
-                    Console.WriteLine($"Your guess is HIGHER than the random number.");
-                    continue;
-                }
-
-                if (guess < randomNumber)
-                {
-                    Console.WriteLine($"Your guess is LESS than the random number.");
-                    continue;
-                }
-
                 if (guess == randomNumber)
                 {
-                    Console.WriteLine($"\n*******Congrats! You correctly guessed the random number: {randomNumber} with {guessCount} attempts*******");
+                    Console.WriteLine($"\n********Congrats! You correctly guessed the random number ({randomNumber}) using {guessCount} attempts********");
                     continueGame = false;
                 }
-                Console.WriteLine("------------------------------------------------------------------");
+                else
+                {
+                    Console.WriteLine("Your guess is {0} than the random number.", guess < randomNumber ? "LOWER" : "HIGHER");
+                }
             }
             else
             {
