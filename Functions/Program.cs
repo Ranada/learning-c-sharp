71d6caa2ac48cfa16ea518458b883c60a2ebad72
Neil Ranada <neil.ranada@gmail.com>
2023-05-24 17:55:02 -0700

    Add example void and return type functions

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        PrintWithPrefix("This is an example with a prefix.");
+        PrintWithPrefix("Let's convert miles to kilometers");
+        
+        Console.WriteLine("6.8 miles converts to {0} km", MilesToKm(6.8f));
+        Console.WriteLine("1.5 miles converts to {0} km", MilesToKm(1.5f));
+    }
+
+    static void PrintWithPrefix(string str)
+    {
+        Console.WriteLine($"::> {str}");
+    }
+
+    static float MilesToKm(float miles)
+    {
+        float result = miles * 1.6f;
+        return result;
     }
 }
