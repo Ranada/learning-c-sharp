52d38f8cd5e04b510e3e29eb3893d64bd58bb330
Neil Ranada <neil.ranada@gmail.com>
2023-05-24 18:13:52 -0700

    Update PrintWithPrefix to use default params or named arguments out of order.

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -4,16 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        PrintWithPrefix("This is an example with a prefix.");
-        PrintWithPrefix("Let's convert miles to kilometers");
+        PrintWithPrefix("This is an example with a default prefix.");
+        PrintWithPrefix("###>", "This is an example with a prefix argument in order.");
+        PrintWithPrefix(str: "This is an example using argument names out of order.", prefix: "%%%>");
+        PrintWithPrefix(":D", "Let's convert miles to kilometers");
         
         Console.WriteLine("6.8 miles converts to {0} km", MilesToKm(6.8f));
         Console.WriteLine("1.5 miles converts to {0} km", MilesToKm(1.5f));
     }
 
-    static void PrintWithPrefix(string str)
+    static void PrintWithPrefix(string str, string prefix = "")
     {
-        Console.WriteLine($"::> {str}");
+        Console.WriteLine($"{prefix}{str}");
     }
 
     static float MilesToKm(float miles)
