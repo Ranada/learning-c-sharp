250cd0d0e0a305a800f1547a811ff1e4d961e99d
Neil Ranada <neil.ranada@gmail.com>
2023-05-25 15:32:57 -0700

    Initial commit

---
+++ IsPalindrome/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace IsPalindrome;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Palindrom Challenge Rules
+            // Program read input and determines if palindrome
+            // Must ignore case and punctuation
+            // Function musbe name IsPalindrome
+            // Returns two values: Boolean and integer
+            // Program runs until "exit" is typed
+
+    }
+}
