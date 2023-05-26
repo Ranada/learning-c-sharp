eb709d8237147d43f66575f446aeef88ec64b06d
Neil Ranada <neil.ranada@gmail.com>
2023-05-25 17:33:03 -0700

    Update formatting

--- IsPalindrome/Program.cs
+++ IsPalindrome/Program.cs
@@ -11,27 +11,41 @@ class Program
         // Returns two values: Boolean and integer
         // Program runs until "exit" is typed
 
+        string? input;
+        do
+        {
+            Console.WriteLine("Enter a string to check if it's a Palindrome:");
+            input = Console.ReadLine();
+            (bool isPalindrome, int length) = IsPalindrome(input);
+            Console.WriteLine($"Palindrome: {isPalindrome}, Length: {length}\n");
+        }
+        while(input != "exit");
+        
         // Test Cases
         // radar -> Palindrome: True, Length: 5
         // No 'X' in Nixon. -> Palindrome: True, Length: 16;
         // A man, a plan, a Canal Panama! -> 
         // Madam, I'm Adam. -> Palindrome: True, Length: 16
         // Hello world -> Palindrome: False, Length: 0
-        // exit
 
-        Console.WriteLine("Palindrome: {0}", IsPalindrome("Radar")); // Expect Palinedrome: True, Length: 5
-        Console.WriteLine(IsPalindrome("No 'X' in Nixon.")); // Palindrome: True, Length: 16;
-        Console.WriteLine(IsPalindrome("A man, a plan, a Canal Panama!")); // Palindrome: True, Length: 30
-        Console.WriteLine(IsPalindrome("Madam, I'm Adam")); // Palindrome: True, Length: 16
-        Console.WriteLine(IsPalindrome("Hello world")); // Hello world -> Palindrome: False, Length: 0
+        // Console.WriteLine("Palindrome: {0}", IsPalindrome("Radar")); // Expect Palinedrome: True, Length: 5
+        // Console.WriteLine(IsPalindrome("No 'X' in Nixon.")); // Palindrome: True, Length: 16;
+        // Console.WriteLine(IsPalindrome("A man, a plan, a Canal Panama!")); // Palindrome: True, Length: 30
+        // Console.WriteLine(IsPalindrome("Madam, I'm Adam")); // Palindrome: True, Length: 16
+        // Console.WriteLine(IsPalindrome("Hello world")); // Hello world -> Palindrome: False, Length: 0
 
         // No 'X' in Nixon.
         // l
         //                r
     }
 
-    static (bool, int) IsPalindrome(string str)
+    static (bool, int) IsPalindrome(string? str)
     {
+        if (str == null || str.Length == 0)
+        {
+            return (false, 0);
+        }
+        
         int l = 0;
         int r = str.Length - 1;
 
@@ -42,7 +56,7 @@ class Program
 
             if (isAlphabet(leftChar) && isAlphabet(rightChar))
             {
-                if (leftChar != rightChar) return (false, str.Length);
+                if (leftChar != rightChar) return (false, 0);
                 l++;
                 r--;
                 continue;
