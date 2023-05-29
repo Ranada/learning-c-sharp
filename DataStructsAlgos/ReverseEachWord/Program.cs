795054c2bd76cd9fac1346a56bb91127f8df500d
Neil Ranada <neil.ranada@gmail.com>
2023-05-29 16:29:49 -0700

    Initial commit

---
+++ DataStructsAlgos/ReverseEachWord/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ReverseEachWord;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!"); 
+    }
+
+    public string ReverseEachWord(string sentence) {
+        // Create a new string builder
+        // Split sentence into an array of words
+        // Loop through the array of words
+            // Reverse each word
+            // Add the word to the string builder
+            // If it is not the last word in the array, add a space
+        // Return the string builder converted to a string
+        return sentence;
+    }
+}
