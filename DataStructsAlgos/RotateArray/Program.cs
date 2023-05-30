a89a9d6002ddc9300000dc4e69bf6b6f6799dcaf
Neil Ranada <neil.ranada@gmail.com>
2023-05-29 19:56:15 -0700

    Implement solution for rotating array right

--- DataStructsAlgos/RotateArray/Program.cs
+++ DataStructsAlgos/RotateArray/Program.cs
@@ -4,27 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int[] input = { 1, 2, 3, 4, 5, 6 };
 
         Console.WriteLine("BEFORE");
-        Console.WriteLine(String.Join(",", arr) + "\n");
+        Console.WriteLine(String.Join(",", input) + "\n");
 
-        RotateArrayLeft(arr);
-        RotateArrayLeft(arr);
+        // RotateArrayLeft(input);
+        // RotateArrayLeft(input);
+
+        RotateArrayRight(input);
+        RotateArrayRight(input);
 
         Console.WriteLine("AFTER");
-        Console.WriteLine(String.Join(",", arr) + "\n");
+        Console.WriteLine(String.Join(",", input) + "\n");
     }
 
-    static void RotateArrayLeft(int[] arr)
+    static void RotateArrayLeft(int[] input)
     {
-        int temp = arr[0];
+        int temp = input[0];
 
-        for (int index = 0; index < arr.Length - 1; index++)
+        for (int index = 0; index < input.Length - 1; index++)
         {
-            arr[index] = arr[index + 1];
+            input[index] = input[index + 1];
         }
-        arr[arr.Length - 1] = temp;
+        input[input.Length - 1] = temp;
+    }
+
+    static void RotateArrayRight(int[] input)
+    {
+        int temp = input[input.Length - 1];
+
+        for (int index = input.Length - 1; index > 0; index--)
+        {
+            input[index] = input[index - 1];
+        }
+        input[0] = temp;
     }
 }
 
