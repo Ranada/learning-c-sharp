8d5697f2990f70cf204e9d97f762a4f4db0a7a2a
Neil Ranada <neil.ranada@gmail.com>
2023-05-29 17:44:56 -0700

    Reverse each word challenge complete

--- DataStructsAlgos/ReverseEachWord/Program.cs
+++ DataStructsAlgos/ReverseEachWord/Program.cs
@@ -1,20 +1,56 @@
-﻿namespace ReverseEachWord;
+﻿using System.Text;
+namespace ReverseEachWord;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!"); 
+        Console.WriteLine(ReverseEachWord("Taylor is a great worker"));
+        Console.WriteLine(ReverseEachWord(null));
+        Console.WriteLine(ReverseEachWord("racer racecar madam"));
+        Console.WriteLine(ReverseEachWord("what can I do today"));
+        Console.WriteLine(ReverseEachWord(" "));
     }
 
-    public string ReverseEachWord(string sentence) {
-        // Create a new string builder
-        // Split sentence into an array of words
-        // Loop through the array of words
-            // Reverse each word
-            // Add the word to the string builder
-            // If it is not the last word in the array, add a space
-        // Return the string builder converted to a string
-        return sentence;
+    static String? ReverseEachWord(String? sentence)
+    {
+        if (string.IsNullOrEmpty(sentence))
+        {
+            return sentence;
+        }
+
+        StringBuilder result = new StringBuilder(sentence.Length);
+        string[] wordsArray = sentence.Split(" ");
+
+        for (int index = 0; index < wordsArray.Length; index++)
+        {
+            String word = wordsArray[index];
+            result.Append(Reverse(word));
+
+            if (index != wordsArray.Length - 1)
+            {
+                result.Append(" ");
+            }
+        }
+
+        return result.ToString();
     }
+    static String Reverse(String word)
+    {
+        if (string.IsNullOrEmpty(word))
+        {
+            return word;
+        }
+
+        StringBuilder reversed = new StringBuilder(word.Length);
+
+        for (int index = word.Length - 1; index >= 0; index--)
+        {
+            char c = word[index];
+            reversed.Append(c);
+        }
+
+        return reversed.ToString();
+    }
+
 }
