babc83e045e2d4d80023309d03b6cf4210a53580
Neil Ranada <neil.ranada@gmail.com>
2023-05-29 19:31:39 -0700

    Initial commit for RotateArray

---
+++ DataStructsAlgos/RotateArray/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RotateArray;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = {1,2,3,4,5,6};
+        Array.ForEach(arr, item => Console.WriteLine(item));
+    }
+}
