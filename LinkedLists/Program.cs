02a268b69fc6948cb6a4eb9f1cbd84924aaf9f4b
Neil Ranada <neil.ranada@gmail.com>
2023-05-30 13:16:53 -0700

    Initial commit for custom linked list

---
+++ LinkedLists/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LinkedLists;
+
+class Program
+{
+
+    public class CustomLinkedList
+    {
+        Node? head;
+
+        public class Node
+        {
+            public int? data;
+            public Node? next;
+
+            public Node(int? d)
+            {
+                data = d;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CustomLinkedList linkedList = new CustomLinkedList();
+            Node firstNode = new Node(1);
+            Node secondNode = new Node(2);
+            Node thirdNode = new Node(3);
+            Node fourthNode = new Node(4);
+            Node fifthNode = new Node(5);
+            Node sixthNode = new Node(6);
+
+            linkedList.head = firstNode;
+            firstNode.next = secondNode;
+            secondNode.next = thirdNode;
+            thirdNode.next = fourthNode;
+            fourthNode.next = fifthNode;
+            fifthNode.next = sixthNode;
+
+           Node ptr = linkedList.head;
+
+           
+           while (ptr.next != null)
+           {
+            Console.WriteLine($"Value: {ptr.data}, Next Value: {ptr.next.data}");
+            ptr = ptr.next;
+           }
+
+        }
+    }
+}
+
