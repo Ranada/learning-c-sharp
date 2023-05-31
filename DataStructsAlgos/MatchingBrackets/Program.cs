cb79b8c79cdd14ade4586eb0b8e9f56772287e46
Neil Ranada <neil.ranada@gmail.com>
2023-05-31 10:38:26 -0700

    Matching brackets solution updated

--- DataStructsAlgos/MatchingBrackets/Program.cs
+++ DataStructsAlgos/MatchingBrackets/Program.cs
@@ -4,13 +4,36 @@ class Program
 {
     static bool HasMatchingBrackets(string input)
     {
-        Console.WriteLine(input);
-        return true;
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            if (c == '(')
+            {
+                stack.Push(c);
+            }
+
+            if (c == ')')
+            {
+                if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
     }
-    
+
     static void Main(string[] args)
     {
-        Console.WriteLine(HasMatchingBrackets("(Hello World)"));
-        Console.WriteLine(HasMatchingBrackets("(Hello World"));
+        Console.WriteLine(HasMatchingBrackets("(Hello World)")); // Expect True
+        Console.WriteLine(HasMatchingBrackets("(stuff( more stuff))")); // Expect True
+        Console.WriteLine(HasMatchingBrackets("(Hello World")); // Expect False
+        Console.WriteLine(HasMatchingBrackets("(stuff)))) more stuff)")); // Expect False
     }
 }
