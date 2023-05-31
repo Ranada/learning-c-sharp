caeb68240172c9cf0bd785a8291286e2f87c5b35
Neil Ranada <neil.ranada@gmail.com>
2023-05-31 10:31:57 -0700

    Initial commit

---
+++ DataStructsAlgos/MatchingBrackets/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MatchingBrackets;
+
+class Program
+{
+    static bool HasMatchingBrackets(string input)
+    {
+        Console.WriteLine(input);
+        return true;
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine(HasMatchingBrackets("(Hello World)"));
+        Console.WriteLine(HasMatchingBrackets("(Hello World"));
+    }
+}
