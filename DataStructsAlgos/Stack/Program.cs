3e9d5e4e76c7691e3fb158767c13e9b145690b4e
Neil Ranada <neil.ranada@gmail.com>
2023-05-30 20:46:13 -0700

    Update with lesson solution

--- DataStructsAlgos/Stack/Program.cs
+++ DataStructsAlgos/Stack/Program.cs
@@ -6,68 +6,59 @@ class Program
 {
     static void PrintNextGreaterElement(int[] arr)
     {
-        // If the arr is empty return
-        if (arr == null || arr.Length == 0)
+        Console.WriteLine("-----------------------");
+
+        if (arr.Length == 0)
         {
             Console.WriteLine("Array is empty or null");
             return;
         }
 
-        // If the array length is 1, print "first item -> -1"
-        if (arr.Length == 1)
-        {
-            Console.WriteLine($"{arr[0]} -> -1");
-            return;
-        }
-
-        // Create stack
         Stack<int> stack = new Stack<int>();
-
-        // Add push first item of array to stack
         stack.Push(arr[0]);
 
-        // Loop through array starting from index 1
         for (int index = 1; index < arr.Length; index++)
         {
-            // Peek at the top of the stack
-            int topItem = stack.Peek();
-            int currentItem = arr[index];
+            int current = arr[index];
 
-            // If the current item is greater than peek item
-            if (currentItem > topItem)
+            if (stack.Count > 0)
             {
-                // Print "peek item -> current item"
-                Console.WriteLine($"{topItem} -> {currentItem}");
-                // Pop stack
-                stack.Pop();
-            }
-            else
-            {
-                // Else push the current item to the stack
-                stack.Push(currentItem);
-            }
+                int popped = stack.Pop();
 
-            if (index == arr.Length -1)
-            {
-                stack.Push(currentItem);
+                while (popped < current)
+                {
+                    Console.WriteLine(popped + "->" + current);
+                    if (stack.Count == 0)
+                    {
+                        break;
+                    }
+                    popped = stack.Pop();
+                }
+
+                if (popped > current)
+                {
+                    stack.Push(popped);
+                }
             }
+            stack.Push(current);
         }
 
-        // While the stack is not null
-        // Pop the stack and print "Popped item -> -1"
-        foreach (int num in stack)
+        while(stack.Count > 0)
         {
-            Console.WriteLine($"{num} -> -1");
+            Console.WriteLine(stack.Pop() + "->" + -1);
         }
 
     }
     static void Main(string[] args)
     {
-        int[] arr = new int[] { 15, 8, 4, 10 };
-        // int[] arr = new int[] {2};
-        // int[] arr = new int[] {2, 3};
-        // int[] arr = new int[] {};
+        int[] arr1 = new int[] { 15, 8, 4, 10 };
+        int[] arr2 = new int[] { 2 };
+        int[] arr3 = new int[] { 2, 3 };
+        int[] arr4 = new int[] { };
 
-        PrintNextGreaterElement(arr);
+        PrintNextGreaterElement(arr1);
+        PrintNextGreaterElement(arr2);
+        PrintNextGreaterElement(arr3);
+        PrintNextGreaterElement(arr4);
     }
 }
