e6aa27592d0ca7134cd364f06291f8af79553516
Neil Ranada <neil.ranada@gmail.com>
2023-05-30 20:01:56 -0700

    Add pseudocode

--- DataStructsAlgos/Stack/Program.cs
+++ DataStructsAlgos/Stack/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Stack;
+﻿using System.Collections.Generic;
+
+namespace Stack;
 
 class Program
 {
     static void PrintNextGreaterElement(int[] arr)
     {
-        Console.WriteLine("Inside PrintNextGreaterElement");
+        // If the arr is empty return
+
+        // If the array length is 1 return -1
+
+        // Create stack
+        // Add push first item of array to stack
+
+        // Loop through array starting from index 1
+            // Peek at the top of the stack
+            // If the current item is greater than peek item 
+                // Print "peek item -> current item"
+                // Pop stack
+            // Else push the current item to the stack
+
+        // While the stack is not null
+            // Pop the stack and print "Popped item -> -1"
     }
     static void Main(string[] args)
     {
