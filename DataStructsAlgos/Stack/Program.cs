110398c404aa682af43f5e2315228141ce13d6e1
Neil Ranada <neil.ranada@gmail.com>
2023-05-30 19:52:41 -0700

    Initial commit for Stacks

---
+++ DataStructsAlgos/Stack/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Stack;
+
+class Program
+{
+    static void PrintNextGreaterElement(int[] arr)
+    {
+        Console.WriteLine("Inside PrintNextGreaterElement");
+    }
+    static void Main(string[] args)
+    {
+        int[] arr = new int[] {15, 8, 4, 10};
+        // int[] arr = new int[] {2};
+        // int[] arr = new int[] {2, 3};
+        // int[] arr = new int[] {};
+
+        PrintNextGreaterElement(arr);
+    }
+}
