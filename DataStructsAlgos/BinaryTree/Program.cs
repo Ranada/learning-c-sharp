a85886064a1de31d7713c9b606f266f0301bd2fb
Neil Ranada <neil.ranada@gmail.com>
2023-06-01 12:07:45 -0700

    Initial commit for binary tree

---
+++ DataStructsAlgos/BinaryTree/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BinaryTree;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int[] values = {4, 1, 3, 8, 9, 6};
+
+        foreach (int num in values) {Console.WriteLine(num);}
+    }
+}
