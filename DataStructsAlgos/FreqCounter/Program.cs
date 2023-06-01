b7f679a0249616ba8a13ae3a9435169b230a54bd
Neil Ranada <neil.ranada@gmail.com>
2023-05-31 19:26:45 -0700

    Add solution for frequency counter

---
+++ DataStructsAlgos/FreqCounter/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FreqCounter;
+
+class Program
+{
+    static void DisplayFreqCounter(int[] arr)
+    {
+        Dictionary<int, int> freqCounter = new Dictionary<int, int>();
+
+        foreach (int item in arr)
+        {
+            if (freqCounter.ContainsKey(item))
+            {
+                freqCounter[item]++;
+            }
+            else
+            {
+                freqCounter[item] = 1;
+            }
+        }
+
+        foreach (KeyValuePair<int, int> pair in freqCounter)
+        {
+            Console.WriteLine(pair.Key + "->" + pair.Value + " times");
+        }
+    }
+    
+    static void Main(string[] args)
+    {
+        DisplayFreqCounter(new int[] {1, 1, 1, 3, 3, 5});
+    }
+}
