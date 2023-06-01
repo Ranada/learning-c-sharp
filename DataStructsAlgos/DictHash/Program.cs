35ce527d19b73b18efa6451c76508b5b98a29c52
Neil Ranada <neil.ranada@gmail.com>
2023-05-31 17:34:13 -0700

    Add Employee dictionary

--- DataStructsAlgos/DictHash/Program.cs
+++ DataStructsAlgos/DictHash/Program.cs
@@ -10,8 +10,20 @@ class Program
         Employee employee2 = new Employee("Mike", 2468, "Design");
         Employee employee3 = new Employee("Sara", 7777, "Sales");
 
-        employee1.DisplayInfo();
-        employee2.DisplayInfo();
-        employee3.DisplayInfo();
+        Dictionary<int, Employee> employeesById = new Dictionary<int, Employee>();
+        employeesById.Add(employee1.Id, employee1);
+        employeesById.Add(employee2.Id, employee2);
+        employeesById.Add(employee3.Id, employee3);
+
+        Employee? e;
+        if (employeesById.TryGetValue(123, out e))
+        {
+            e.DisplayInfo();
+        }
+        else
+        {
+            Console.WriteLine("Unable to find info for employee.");
+        }
+
     }
 }
