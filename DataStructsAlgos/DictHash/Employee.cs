362d82b48c1789a645d5e18c9302ebcc603b6f28
Neil Ranada <neil.ranada@gmail.com>
2023-05-31 17:22:59 -0700

    Separate Employee class into separate file

---
+++ DataStructsAlgos/DictHash/Employee.cs
@@ -0,0 +1,34 @@
+class Employee
+{
+    private string _name;
+    private int _id;
+    private string _department;
+    public Employee(string name, int id, string department)
+    {
+        _name = name;
+        _id = id;
+        _department = department;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public string Department
+    {
+        get => _department;
+        set => _department = value;
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Id: {Id} Name: {Name} Department: {Department}");
+    }
+}
