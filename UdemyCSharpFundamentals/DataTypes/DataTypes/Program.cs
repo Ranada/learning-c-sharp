61ca3d1e026467dd4c06ae91df69232f01c373ed
Neil Ranada <neil.ranada@gmail.com>
2023-06-15 14:07:30 -0700

    Initial commit for data types review

---
+++ UdemyCSharpFundamentals/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Integer Data Types");
+
+            sbyte num1 = 1;
+            sbyte num2 = 2;
+            sbyte sum = (sbyte)(num1 + num2);
+            Console.WriteLine($"sbyt sum (sbyte)({num1.ToString()} + {num2.ToString()}) = {sum}");
+            Console.WriteLine("Note: the sbyte num1 and num2 are treated as integers during the sum operation and need to be cast to `sbyte`");
+
+        }
+    }
+}
