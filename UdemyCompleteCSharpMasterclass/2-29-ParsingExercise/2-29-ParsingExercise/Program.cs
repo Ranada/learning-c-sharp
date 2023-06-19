727922bf4961b7762a3402cb31b7c2d541e7ce16
Neil Ranada <neil.ranada@gmail.com>
2023-06-19 15:40:19 -0700

    Data type parsing exercise complete

---
+++ UdemyCompleteCSharpMasterclass/2-29-ParsingExercise/2-29-ParsingExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _2_29_ParsingExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringForFloat = "0.85"; // datatype should be float
+            string stringForInt = "12345"; // datatype should be int
+
+            Console.WriteLine($"{float.Parse(stringForFloat)} is {float.Parse(stringForFloat).GetType()} dataype");
+            Console.WriteLine($"{Int32.Parse(stringForInt)} is {Int32.Parse(stringForInt).GetType()} dataype");
+        }
+    }
+}
