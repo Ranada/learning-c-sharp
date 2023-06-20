22b46534015b3fddddfe4fbdb217db22c4e888f4
Neil Ranada <neil.ranada@gmail.com>
2023-06-20 15:48:03 -0700

    GreetFriends method exercise complete

---
+++ UdemyCompleteCSharpMasterclass/3-47-GreetFriends/3-47-GreetFriends/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _3_47_GreetFriends
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = new string[] { "Chris", "Marc", "Iris" };
+            GreetFriends(friends);
+        }
+
+        public static void GreetFriends(string[] friends)
+        {
+            foreach (string friend in friends)
+            {
+                Console.WriteLine($"Hello, {friend}!");
+            }
+        }
+    }
+}
