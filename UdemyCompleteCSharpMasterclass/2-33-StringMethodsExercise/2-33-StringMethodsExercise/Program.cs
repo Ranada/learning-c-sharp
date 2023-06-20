688505f1011e3a6b60c9cc342dffb6fc5bd77df8
Neil Ranada <neil.ranada@gmail.com>
2023-06-19 17:30:16 -0700

    string methods exercise compelte

---
+++ UdemyCompleteCSharpMasterclass/2-33-StringMethodsExercise/2-33-StringMethodsExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _2_33_StringMethodsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? entry;
+            do {
+                Console.WriteLine("Please enter your name and press enter:");
+                entry = Console.ReadLine();
+
+                Console.WriteLine($"Uppercase: {entry?.ToUpper()}");
+                Console.WriteLine($"Lowercase: {entry?.ToLower()}");
+                Console.WriteLine($"Trimmed leading/ending whitespace: {entry?.Trim()}\n");
+
+                if (entry.Length >= 4) { 
+                    Console.WriteLine($"Substring with length of 4: {entry?.Substring(0, 4)}\n");
+                }
+            } while (entry != "exit");
+        }
+    }
+}
