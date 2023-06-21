0ad5dcd424c432d25c90c6246683b8ff8cf6ce1e
Neil Ranada <neil.ranada@gmail.com>
2023-06-21 14:03:30 -0700

    Try Parse exercise complete

---
+++ UdemyCompleteCSharpMasterclass/3-56-TryParseDemo/3-56-TryParseDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _3_56_TryParseDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string entry = "";
+            int temp = 0;
+            int numTemp = 0;
+
+            do
+            {
+                Console.WriteLine("Enter temperature: ");
+                entry = Console.ReadLine();
+
+                bool success = int.TryParse(entry, out temp);
+
+                if (success)
+                {
+                    numTemp = temp;
+                    Console.WriteLine("String number successfully parsed");
+                }
+                else
+                {
+                    Console.WriteLine("Could not parse entry into an integer");
+                }
+
+                Console.WriteLine();
+
+            } while (entry != "exit");
+        }
+    }
+}
