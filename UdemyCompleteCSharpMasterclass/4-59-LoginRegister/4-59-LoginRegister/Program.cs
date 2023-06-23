f26363debf3666746ad6efa0585caf1e37413c27
Neil Ranada <neil.ranada@gmail.com>
2023-06-23 10:26:10 -0500

    Initial commit for register/login exercise

---
+++ UdemyCompleteCSharpMasterclass/4-59-LoginRegister/4-59-LoginRegister/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+
+namespace _4_59_LoginRegister
+{
+    internal class Program
+    {
+        static string username;
+        static string password;
+        static void Main(string[] args)
+        {
+            Register();
+            Login();
+        }
+
+        public static void Register() {
+            Console.WriteLine("Create username:");
+            username = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Create password:");
+            password = Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        public static void Login()
+        {
+            string usernameEntry = "";
+            string passwordEntry = "";
+
+            while (usernameEntry != username)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter username:");
+                if (username == Console.ReadLine())
+                {
+                    Console.WriteLine("Username is valid.");
+                    Console.WriteLine();
+    
+                    while (passwordEntry != password)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Enter password:");
+                        if (password == Console.ReadLine())
+                        {
+                            Console.WriteLine("Password is valid");
+                            Console.WriteLine("Successful login!");
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Password is incorrect. Try again.");
+                            continue;
+                        }
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Username does no exist. Try again.\n");
+                    continue;
+                }
+            }
+        }
+    }
+}
