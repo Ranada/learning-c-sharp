36c105d28d3212e0056de806bb8fd707f92afe22
Neil Ranada <neil.ranada@gmail.com>
2023-07-03 16:07:55 -0700

    Create game board

--- UdemyCompleteCSharpMasterclass/5-7-TicTacToe/5-7-TicTacToe/Program.cs
+++ UdemyCompleteCSharpMasterclass/5-7-TicTacToe/5-7-TicTacToe/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        public static string[,] board = {
+                                            {"1", "2", "3"},
+                                            {"4", "5", "6"},
+                                            {"7", "8", "9"}
+                                        };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintBoard();
+        }
+
+        public static void PrintBoard()
+        {
+            Console.WriteLine("     |     |    ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("_____|_____|_____");
+
+            Console.WriteLine("     |     |    ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 4, 5, 6);
+            Console.WriteLine("_____|_____|_____");
+
+            Console.WriteLine("     |     |    ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 7, 8, 9);
+            Console.WriteLine("     |     |    ");
         }
     }
 }
