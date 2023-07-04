3e36fdb7078f87ee9a415140bf8534af9fabda04
Neil Ranada <neil.ranada@gmail.com>
2023-07-03 20:16:19 -0700

    Add win condition

--- UdemyCompleteCSharpMasterclass/5-7-TicTacToe/5-7-TicTacToe/Program.cs
+++ UdemyCompleteCSharpMasterclass/5-7-TicTacToe/5-7-TicTacToe/Program.cs
@@ -21,8 +21,14 @@
                 CheckCurrentPlayer();
                 GetCurrentPlayerInput();
 
+                if (HaveWinner())
+                {
+                    PrintBoard();
+                    Console.WriteLine($"**** Player {currentPlayer} wins! *****\n");
+                    return;
+                }
                 // Check if there is a winner
-                    // If there is a winner, print a message for the winner and end the game
+                // If there is a winner, print a message for the winner and end the game
                 // If all the turnCount are used and there is no winner, print a message saying no one won, and reset the game
             } while (turnCount < 9);
             PrintBoard();
@@ -34,16 +40,16 @@
             {
                 currentPlayer = 1;
             }
-            else 
-            { 
+            else
+            {
                 currentPlayer = 2;
             }
         }
 
-        public static void GetCurrentPlayerInput() 
+        public static void GetCurrentPlayerInput()
         {
-            do 
-            { 
+            do
+            {
                 Console.Write($"Player {currentPlayer}, pick an available numbered position (1 through 9): ");
                 input = Console.ReadLine()!;
                 if (input == null) input = "";
@@ -67,8 +73,9 @@
                 isValidNumber = true;
             }
             else
-            { 
-                Console.WriteLine("You must enter an available number between 0 and 9.\n");
+            {
+                Console.WriteLine("*** OOPS! You must enter an available number position between 0 and 9.***\n");
+                PrintBoard();
                 return false;
             }
 
@@ -84,10 +91,11 @@
                     (input == "8" && board[2, 1] != "8") ||
                     (input == "9" && board[2, 2] != "9")
                     )
-                 {
-                    Console.WriteLine("Oops! This position is already filled. Select an available position number by choosing a visible number between 0 and 9.\n");
+                {
+                    Console.WriteLine("*** OOPS! This position is already filled. Select an available number position between 0 and 9. ***\n");
+                    PrintBoard();
                     return false;
-                 }
+                }
 
             }
             return true;
@@ -99,22 +107,86 @@
 
             if (currentPlayer == 1) gamePiece = "X";
             else if (currentPlayer == 2) gamePiece = "O";
-            
-            if (input == "1") board[0,0] = gamePiece;
-            if (input == "2") board[0,1] = gamePiece;
-            if (input == "3") board[0,2] = gamePiece;
-            if (input == "4") board[1,0] = gamePiece;
-            if (input == "5") board[1,1] = gamePiece;
-            if (input == "6") board[1,2] = gamePiece;
-            if (input == "7") board[2,0] = gamePiece;
-            if (input == "8") board[2,1] = gamePiece;
-            if (input == "9") board[2,2] = gamePiece;
+
+            if (input == "1") board[0, 0] = gamePiece;
+            if (input == "2") board[0, 1] = gamePiece;
+            if (input == "3") board[0, 2] = gamePiece;
+            if (input == "4") board[1, 0] = gamePiece;
+            if (input == "5") board[1, 1] = gamePiece;
+            if (input == "6") board[1, 2] = gamePiece;
+            if (input == "7") board[2, 0] = gamePiece;
+            if (input == "8") board[2, 1] = gamePiece;
+            if (input == "9") board[2, 2] = gamePiece;
         }
+
+
+        public static bool HaveWinner()
+        {
+            if (IsHorizontalWin() || IsVerticalWin() || IsDiagonalWin())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsHorizontalWin()
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                if (
+                    (board[row, 0] == "X" && board[row, 1] == "X" && board[row, 2] == "X") ||
+                    (board[row, 0] == "O" && board[row, 1] == "O" && board[row, 2] == "O")
+                    )
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool IsVerticalWin()
+        {
+            for (int col = 0; col < board.GetLength(1); col++)
+            {
+                if (
+                    (board[0, col] == "X" && board[1, col] == "X" && board[2, col] == "X") ||
+                    (board[0, col] == "O" && board[1, col] == "O" && board[2, col] == "O")
+                    )
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool IsDiagonalWin()
+        {
+
+            if (
+                (board[0, 0] == "X" && board[1, 1] == "X" && board[2, 2] == "X") ||
+                (board[0, 0] == "O" && board[1, 1] == "O" && board[2, 2] == "O")
+                )
+            {
+                return true;
+            }
+
+            if (
+                (board[0, 2] == "X" && board[1, 1] == "X" && board[2, 0] == "X") ||
+                (board[0, 2] == "O" && board[1, 1] == "O" && board[2, 0] == "O")
+                )
+            {
+                return true;
+            }
+
+            return false;
+        }
+
 
         public static void PrintBoard()
         {
             Console.WriteLine("     |     |    ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[0,0], board[0,1], board[0, 2]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[0, 0], board[0, 1], board[0, 2]);
             Console.WriteLine("_____|_____|_____");
 
             Console.WriteLine("     |     |    ");
