35943c93ae0333a3bcf8d88de5321d873ea2ae93
Neil Ranada <neil.ranada@gmail.com>
2023-07-03 19:55:40 -0700

    Add ability to check valid player input

--- UdemyCompleteCSharpMasterclass/5-7-TicTacToe/5-7-TicTacToe/Program.cs
+++ UdemyCompleteCSharpMasterclass/5-7-TicTacToe/5-7-TicTacToe/Program.cs
@@ -2,29 +2,25 @@
 {
     internal class Program
     {
-        //public static bool continueGame = true;
         public static int currentPlayer = 2;
         public static int turnCount = 0;
-        public static string? input = "";
+        public static string input = "";
+
         public static string[,] board = {
                                             {"1", "2", "3"},
                                             {"4", "5", "6"},
                                             {"7", "8", "9"}
                                         };
+
         static void Main(string[] args)
         {
 
             do
             {
                 PrintBoard();
-                // Track number of turnCount
-                // Check player turn
                 CheckCurrentPlayer();
                 GetCurrentPlayerInput();
-                // Ask current player to pick a number position on the game board
-                    // If it is player 1's turn, replace number with "X" for player 1 and increment the number of turnCount
-                    // If it is plasyter 2's turn, replace number with "O" for player 2 and increment the number of turnCount
-                // Print the updated game board
+
                 // Check if there is a winner
                     // If there is a winner, print a message for the winner and end the game
                 // If all the turnCount are used and there is no winner, print a message saying no one won, and reset the game
@@ -46,14 +42,55 @@
 
         public static void GetCurrentPlayerInput() 
         {
-            Console.Write($"Player {currentPlayer}, pick an available numbered position (1 through 9): ");
-            input = Console.ReadLine();
-            Console.WriteLine();
-
-            if (input == null) input = "";
+            do 
+            { 
+                Console.Write($"Player {currentPlayer}, pick an available numbered position (1 through 9): ");
+                input = Console.ReadLine()!;
+                if (input == null) input = "";
+                Console.WriteLine();
+            } while (!isValid(ref input));
 
             UpdateBoard(input);
             turnCount++;
+        }
+
+        public static bool isValid(ref string input)
+        {
+            bool isInteger = false;
+            int number = 0;
+            bool isValidNumber = false;
+
+            isInteger = int.TryParse(input, out number);
+
+            if (isInteger && (number >= 0 && number <= 9))
+            {
+                isValidNumber = true;
+            }
+            else
+            { 
+                Console.WriteLine("You must enter an available number between 0 and 9.\n");
+                return false;
+            }
+
+            if (isValidNumber)
+            {
+                if ((input == "1" && board[0, 0] != "1") ||
+                    (input == "2" && board[0, 1] != "2") ||
+                    (input == "3" && board[0, 2] != "3") ||
+                    (input == "4" && board[1, 0] != "4") ||
+                    (input == "5" && board[1, 1] != "5") ||
+                    (input == "6" && board[1, 2] != "6") ||
+                    (input == "7" && board[2, 0] != "7") ||
+                    (input == "8" && board[2, 1] != "8") ||
+                    (input == "9" && board[2, 2] != "9")
+                    )
+                 {
+                    Console.WriteLine("Oops! This position is already filled. Select an available position number by choosing a visible number between 0 and 9.\n");
+                    return false;
+                 }
+
+            }
+            return true;
         }
 
         public static void UpdateBoard(string input)
