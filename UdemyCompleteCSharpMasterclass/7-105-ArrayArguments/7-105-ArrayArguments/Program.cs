5dd63b7f25ca552a7e01c919eb79bc6a3052d4ac
Neil Ranada <neil.ranada@gmail.com>
2023-07-05 15:09:44 -0700

    Solution attempt for array argument exercise

---
+++ UdemyCompleteCSharpMasterclass/7-105-ArrayArguments/7-105-ArrayArguments/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _7_105_ArrayArguments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] happiness = { 1, 5, 99, 1000, 50, 24 };
+            SunIsShining(happiness);
+        }
+
+        public static void SunIsShining(int[] happiness)
+        {
+            Console.WriteLine("Let's boost the happiness!");
+            foreach (int num in happiness) {
+                Console.WriteLine($"{num} -> {num + 2}");
+            }
+        }
+    }
+}
