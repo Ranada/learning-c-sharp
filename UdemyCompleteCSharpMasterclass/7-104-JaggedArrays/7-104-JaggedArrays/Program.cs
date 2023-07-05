dd08f440ce7dc3f631bca060341ceba4e41b196a
Neil Ranada <neil.ranada@gmail.com>
2023-07-05 14:44:16 -0700

    Jagged array solution attempt

---
+++ UdemyCompleteCSharpMasterclass/7-104-JaggedArrays/7-104-JaggedArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _7_104_JaggedArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] friendsFamily = new string[][]
+            {
+                new string[] {"Chris", "Jason", "Janice"},
+                new string[] { "Marc", "Don", "Ariel"},
+                new string[] {"Mupri", "Eric"}
+            };
+
+            foreach (string[] row in friendsFamily)
+            {
+                string friend = row[0];
+                Console.Write($"{friend}'s famiy members are ");
+                for (int i = 1; i < row.Length; i++)
+                {
+                    string familyMember = row[i];
+                    Console.Write($"{familyMember}");
+
+                    if (i != row.Length - 1) { 
+                        Console.Write(", ");
+                    }
+                }
+                Console.Write(".\n");
+            }
+        }
+    }
+}
