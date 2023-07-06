b6d76792cc39b2fe411b81e7b6a1b567a3d5db34
Neil Ranada <neil.ranada@gmail.com>
2023-07-05 21:52:13 -0700

    Initial commit for hash tables exercise

---
+++ UdemyCompleteCSharpMasterclass/7-114-HashTables/7-114-HashTables/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _7_114_HashTables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud1 = new Student(1, "Neil", 4.0f);
+            Console.WriteLine($"{stud1.Gpa}");
+        }
+    }
+}
---
+++ UdemyCompleteCSharpMasterclass/7-114-HashTables/7-114-HashTables/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_114_HashTables
+{
+    internal class Student
+    {
+        private int _id;
+        private string _name;
+        private float _gpa;
+
+        public Student(int id, string name, float gpa)
+        { 
+            this._id = id;
+            this._name = name;
+            this._gpa = gpa;
+        }
+
+        public int Id
+        { 
+            get => _id; set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name; set => _name = value;
+        }
+
+        public float Gpa
+        {
+            get => _gpa; set => _gpa = value;
+        }
+    }
+}
