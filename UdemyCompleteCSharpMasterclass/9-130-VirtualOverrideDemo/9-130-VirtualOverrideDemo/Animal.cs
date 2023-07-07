04353ad37db76621f94663c1c1ce53eed84d08a9
Neil Ranada <neil.ranada@gmail.com>
2023-07-06 20:26:36 -0700

    Initial commit for virtual override demo

---
+++ UdemyCompleteCSharpMasterclass/9-130-VirtualOverrideDemo/9-130-VirtualOverrideDemo/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_130_VirtualOverrideDemo
+{
+    internal class Animal
+    {
+        private string _name;
+        private int _age;
+        private bool _isHungry;
+
+        public Animal(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Animal Sound!");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Eating");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Playing");
+        }
+
+
+
+
+    }
+}
---
+++ UdemyCompleteCSharpMasterclass/9-130-VirtualOverrideDemo/9-130-VirtualOverrideDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _9_130_VirtualOverrideDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
