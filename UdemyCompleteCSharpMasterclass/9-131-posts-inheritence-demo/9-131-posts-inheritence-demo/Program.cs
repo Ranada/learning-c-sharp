f8f66848e62c33ef99a474a647543580cdc20c9a
Neil Ranada <neil.ranada@gmail.com>
2023-07-22 18:20:48 -0700

    Initial commit for post inheritence demo

---
+++ UdemyCompleteCSharpMasterclass/9-131-posts-inheritence-demo/9-131-posts-inheritence-demo/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _9_131_posts_inheritence_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string? entry = "";
+
+            do
+            {
+                Console.WriteLine("Enter type of post you want to create ('text', 'image', or 'video'): ");
+                Console.WriteLine("Or enter 'q' or 'quit' to exit the app.\n");
+                Console.Write("> ");
+                entry = Console.ReadLine();
+                if (entry != null)
+                {
+                    Console.WriteLine($"{entry}\n");
+                }
+            } while (entry != "q" && entry != "quit");
+
+            Console.WriteLine("Exiting the program");
+
+            /*
+             * General Post
+             * - ID
+             * - Title
+             * - Author
+             * - Post date
+             * 
+             * Image post
+             * - Inherit general post
+             * - Image URL
+             * 
+             * Video post
+             * - Inherit general post
+             * 
+             */
+        }
+    }
+}
