4f2fdbf79b8039545e0508cadfbe4108867f3a16
Neil Ranada <neil.ranada@gmail.com>
2023-07-26 17:09:33 -0400

    Practice creating feature starting with test requirement first

--- UdemyCompleteCSharpMasterclass/22-288-CalculatorUnitTestDemo/22-288-CalculatorUnitTestDemo/UnitTest1.cs
+++ UdemyCompleteCSharpMasterclass/22-288-CalculatorUnitTestDemo/22-288-CalculatorUnitTestDemo/UnitTest1.cs
@@ -5,9 +5,14 @@ namespace _22_288_CalculatorUnitTestDemo
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Sum_of_2_and_2_should_be_4()
         {
-
+            Calculator calculator = new Calculator();
+            var result = calculator.Sum(2, 2);
+            if (result != 4)
+                throw new Exception();
         }
+
+        
     }
 }
