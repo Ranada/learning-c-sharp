35d701c877f8330ebd98f7e08a4969e5aa71a665
Neil Ranada <neil.ranada@gmail.com>
2023-07-27 11:29:15 -0400

    Refactor Sum() method and rename test class

--- UdemyCompleteCSharpMasterclass/22-288-CalculatorUnitTestDemo/22-288-CalculatorUnitTestDemo/CalculatorTests.cs
+++ UdemyCompleteCSharpMasterclass/22-288-CalculatorUnitTestDemo/22-288-CalculatorUnitTestDemo/CalculatorTests.cs
@@ -3,7 +3,7 @@ using FluentAssertions;
 
 namespace _22_288_CalculatorUnitTestDemo
 {
-    public class UnitTest1
+    public class CalculatorTests
     {
         [Fact]
         public void Sum_of_2_and_2_should_be_4()
