0653510e7ffe863e730953f16458696c66ecd6bf
Neil Ranada <neil.ranada@gmail.com>
2023-07-27 17:09:29 -0400

    Create failing test for Book_1_seat_SeatCapacity_should_be_2()

--- UdemyCompleteCSharpMasterclass/22-295-FlightBookingUnitTests/22-295-FlightBookingUnitTests/UnitTest1.cs
+++ UdemyCompleteCSharpMasterclass/22-295-FlightBookingUnitTests/22-295-FlightBookingUnitTests/UnitTest1.cs
@@ -1,13 +1,16 @@
+using Domain.Tests;
+using FluentAssertions;
+
 namespace _22_295_FlightBookingUnitTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Book_1_seat_SeatCapacity_should_be_2()
         {
             var flight = new Flight(seatCapacity: 3);
             flight.Book("neil.ranada@outlook.com", 1);
-            flight.RemainingNumberOfSeats.ShouldBe(2);
+            flight.RemainingNumberOfSeats.Should().Be(2);
         }
     }
 }
---
+++ UdemyCompleteCSharpMasterclass/22-295-FlightBookingUnitTests/Domain.Tests/Flight.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Tests
+{
+    public class Flight
+    {
+        private int _remainingNumberOfSeats;
+        public Flight(int seatCapacity) { 
+            RemainingNumberOfSeats = seatCapacity;
+        }
+
+        public void Book(string email, int seats)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemainingNumberOfSeats
+        {
+            get => _remainingNumberOfSeats;
+            set => _remainingNumberOfSeats = value;
+        }
+    }
+}
