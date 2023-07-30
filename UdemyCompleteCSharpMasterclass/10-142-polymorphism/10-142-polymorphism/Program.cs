3f1c69f282c7500db20b8b9f8fa2255e3a3e7060
Neil Ranada <neil.ranada@gmail.com>
2023-07-30 14:51:05 -0700

    Initial commit for polymorphism exercise

---
+++ UdemyCompleteCSharpMasterclass/10-142-polymorphism/10-142-polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _10_142_polymorphism
+{
+    internal class Program
+    {
+        // Create a base class Car with two properties HP and Color
+        // Create a Constructor setting those two properties
+        // Create a Method called ShowDetails() which shos th HP and Color of the car on the console
+        // Create a Repair Method which writes "Car was repaired!" onto the console
+        // Create two deriving classes, BMW and Audi, which have their own constructor and have an additional property
+        // called Model. Also a privat member called brand. Brand should be different in each of the two classes.
+        // Create the two methods ShowDetails() and Repair in them as well. Adjust those methods accordingly.
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
