aa0206a0d6270f8978f1b7dbe3b99da1399e065e
Neil Ranada <neil.ranada@gmail.com>
2023-08-02 16:43:12 -0700

    Add custom pizzas model view

--- UdemyAspNetCoreMVCRazorBlazorEntity/4-35-RazorPizzeria/4-35-RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ UdemyAspNetCoreMVCRazorBlazorEntity/4-35-RazorPizzeria/4-35-RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,3 +1,4 @@
+using _4_35_RazorPizzeria.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,8 @@ namespace _4_35_RazorPizzeria.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizzas { get; set; }
         public void OnGet()
         {
         }
