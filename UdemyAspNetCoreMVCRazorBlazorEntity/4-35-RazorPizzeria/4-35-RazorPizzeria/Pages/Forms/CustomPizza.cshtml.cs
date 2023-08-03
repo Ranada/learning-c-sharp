88b1f069158524d5a4fbc720d132515909306267
Neil Ranada <neil.ranada@gmail.com>
2023-08-02 17:29:37 -0700

    Add pizza form content and update styling

--- UdemyAspNetCoreMVCRazorBlazorEntity/4-35-RazorPizzeria/4-35-RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ UdemyAspNetCoreMVCRazorBlazorEntity/4-35-RazorPizzeria/4-35-RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -7,7 +7,7 @@ namespace _4_35_RazorPizzeria.Pages.Forms
     public class CustomPizzaModel : PageModel
     {
         [BindProperty]
-        public PizzasModel Pizzas { get; set; }
+        public PizzasModel Pizza { get; set; }
         public void OnGet()
         {
         }
