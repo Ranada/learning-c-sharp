ee68d755762f02893fe47bc30b083579dc4a082e
Neil Ranada <neil.ranada@gmail.com>
2023-08-04 14:51:30 -0700

    Add Orders page with table populated by database

---
+++ UdemyAspNetCoreMVCRazorBlazorEntity/4-35-RazorPizzeria/4-35-RazorPizzeria/Pages/Orders.cshtml.cs
@@ -0,0 +1,23 @@
+using _4_35_RazorPizzeria.Data;
+using _4_35_RazorPizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _4_35_RazorPizzeria.Pages
+{
+    public class OrdersModel : PageModel
+    {
+        public List<PizzaOrder> PizzaOrders = new List<PizzaOrder>();
+        private readonly ApplicationDbContext _context;
+        
+        public OrdersModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public void OnGet()
+        {
+            PizzaOrders = _context.PizzaOrders.ToList();
+        }
+    }
+}
