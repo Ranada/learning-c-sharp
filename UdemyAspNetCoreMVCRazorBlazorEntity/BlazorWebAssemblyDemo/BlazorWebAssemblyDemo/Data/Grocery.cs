c0f70fbca84a05a7f04b65ea827855092e7f0863
Neil Ranada <neil.ranada@gmail.com>
2023-08-05 11:34:05 -0700

    Add ability to add groceries by name and price and display list with total price

---
+++ UdemyAspNetCoreMVCRazorBlazorEntity/BlazorWebAssemblyDemo/BlazorWebAssemblyDemo/Data/Grocery.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorWebAssemblyDemo.Data
+{
+    public class Grocery
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Grocery(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
