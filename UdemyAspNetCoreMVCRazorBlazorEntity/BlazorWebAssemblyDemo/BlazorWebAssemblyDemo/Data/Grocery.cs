5eac18ebd6624ca8a36aac9c8bdfd61f8eb50ecf
Neil Ranada <neil.ranada@gmail.com>
2023-08-07 17:16:15 -0700

    Refactor using built-in blazor EditForm component and validation syntax

--- UdemyAspNetCoreMVCRazorBlazorEntity/BlazorWebAssemblyDemo/BlazorWebAssemblyDemo/Data/Grocery.cs
+++ UdemyAspNetCoreMVCRazorBlazorEntity/BlazorWebAssemblyDemo/BlazorWebAssemblyDemo/Data/Grocery.cs
@@ -1,11 +1,18 @@
-﻿namespace BlazorWebAssemblyDemo.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAssemblyDemo.Data
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should be less than 15 characters.")]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1,10000, ErrorMessage = "Valid price range is (1-10000)")]
         public decimal Price { get; set; }
 
-        public Grocery(string name, decimal price)
+        public Grocery(string name = "", decimal price = 0)
         {
             Name = name;
             Price = price;
