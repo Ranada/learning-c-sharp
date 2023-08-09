e53a8bd553574e6154aac7d90a8fca7241b9b634
Neil Ranada <neil.ranada@gmail.com>
2023-08-09 15:06:31 -0700

    Add a ability to preview image thumbnails and pdf logo for selected files

--- UdemyAspNetCoreMVCRazorBlazorEntity/5-68-FileUploadDemo/5-68-FileUploadDemo/Services/FileUpload.cs
+++ UdemyAspNetCoreMVCRazorBlazorEntity/5-68-FileUploadDemo/5-68-FileUploadDemo/Services/FileUpload.cs
@@ -5,6 +5,7 @@ namespace _5_68_FileUploadDemo.Services
     public interface IFileUpload
     {
         Task UploadFile(IBrowserFile file);
+        Task<string> GeneratePreviewUrl(IBrowserFile file);
     }
     public class FileUpload : IFileUpload
     {
@@ -35,6 +36,23 @@ namespace _5_68_FileUploadDemo.Services
                     _logger.LogError(ex.ToString());
                 }
             }
+        }
+
+        public async Task<string> GeneratePreviewUrl(IBrowserFile file)
+        {
+            if (!file.ContentType.Contains("image"))
+            {
+                if (file.ContentType.Contains("pdf"))
+                {
+                    return "images/pdf_logo.png";
+                }
+            }
+
+            var resizedImage = await file.RequestImageFileAsync(file.ContentType, 100, 100);
+            var buffer = new byte[resizedImage.Size];
+            await resizedImage.OpenReadStream().ReadAsync(buffer);
+            return $"data:{file.ContentType};base64,{Convert.ToBase64String(buffer)}";
+
         }
     }
 }
