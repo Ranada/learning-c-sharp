5b18fc3a1f8eefc0d97ee1a00c324919f9661b71
Neil Ranada <neil.ranada@gmail.com>
2023-08-11 15:58:37 -0700

    Add download functionality

--- UdemyAspNetCoreMVCRazorBlazorEntity/5-68-FileUploadDemo/5-68-FileUploadDemo/Services/FileDownload.cs
+++ UdemyAspNetCoreMVCRazorBlazorEntity/5-68-FileUploadDemo/5-68-FileUploadDemo/Services/FileDownload.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.JSInterop;
 
 namespace _5_68_FileUploadDemo.Services
 {
     public interface IFileDownload
     {
         Task<List<string>> GetUploadedFiles();
+        Task DownloadFile(string url);
     }
     public class FileDownload : IFileDownload
     {
         private IWebHostEnvironment _webHostEnvironment;
+        private readonly IJSRuntime _js;
+
+        public FileDownload(IWebHostEnvironment webHostEnvironment, IJSRuntime js)
+        { 
+            _webHostEnvironment = webHostEnvironment;
+            _js = js;
+        }
         public async Task<List<string>> GetUploadedFiles()
         {
             var base64Urls = new List<string>();
@@ -32,6 +41,11 @@ namespace _5_68_FileUploadDemo.Services
             }
 
             return base64Urls;
+        }
+
+        public async Task DownloadFile(string url)
+        { 
+            await _js.InvokeVoidAsync("downloadFile", url);
         }
 
         private string GetMimeTypeForFileExtension(string filePath)
