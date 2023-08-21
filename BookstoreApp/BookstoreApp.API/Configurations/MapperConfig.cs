29d54df5a6409bb3787d0b82ef03c680de301d7c
Neil Ranada <neil.ranada@gmail.com>
2023-08-20 22:56:34 -0700

    Add BookReadOnlyDto and implement AutoMapper

--- BookstoreApp/BookstoreApp.API/Configurations/MapperConfig.cs
+++ BookstoreApp/BookstoreApp.API/Configurations/MapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BookstoreApp.API.Data;
 using BookstoreApp.API.Models.Author;
+using BookstoreApp.API.Models.Book;
 
 namespace BookstoreApp.API.Configurations
 {
@@ -11,6 +12,7 @@ namespace BookstoreApp.API.Configurations
             CreateMap<AuthorCreateDto, Author>().ReverseMap();
             CreateMap<AuthorUpdateDto, Author>().ReverseMap();
             CreateMap<AuthorReadOnlyDto, Author>().ReverseMap();
+            CreateMap<BookReadOnlyDto, Book>().ReverseMap();
         }
     }
 }
