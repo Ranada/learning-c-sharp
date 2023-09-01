9de10c2383332213205410e09796b9039f273859
Neil Ranada <neil.ranada@gmail.com>
2023-08-31 23:55:35 -0700

    Add authentication service

---
+++ BookstoreApp/BookstoreApp.Blazor.Server.UI/HttpServices/Authentication/AuthenticationService.cs
@@ -0,0 +1,19 @@
+﻿using BookstoreApp.Blazor.Server.UI.HttpServices.Base;
+
+namespace BookstoreApp.Blazor.Server.UI.HttpServices.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IClient httpClient;
+
+        public AuthenticationService(IClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public Task<bool> AuthenticateAsync(LoginUserDto loginModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ BookstoreApp/BookstoreApp.Blazor.Server.UI/HttpServices/Authentication/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using BookstoreApp.Blazor.Server.UI.HttpServices.Base;
+
+namespace BookstoreApp.Blazor.Server.UI.HttpServices.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> AuthenticateAsync(LoginUserDto loginModel);
+    }
+
+}
