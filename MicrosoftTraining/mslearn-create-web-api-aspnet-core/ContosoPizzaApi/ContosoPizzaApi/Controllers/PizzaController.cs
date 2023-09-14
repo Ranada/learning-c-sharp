1d6ff288ef64c0f17bc25a11dfb072c1251961fe
Neil Ranada <neil.ranada@gmail.com>
2023-09-13 20:42:15 -0700

    Add pizza controller

---
+++ MicrosoftTraining/mslearn-create-web-api-aspnet-core/ContosoPizzaApi/ContosoPizzaApi/Controllers/PizzaController.cs
@@ -0,0 +1,27 @@
+﻿using ContosoPizzaApi.Models;
+using ContosoPizzaApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizzaApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController() 
+    { 
+    
+    }
+
+    // GET all action
+
+    // GET by Id action
+
+    // POST action
+
+    // PUT action
+
+    // DELETE action
+
+
+}
