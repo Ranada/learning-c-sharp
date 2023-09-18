c625cca8ca5124a333b21bd7031ca76b3105c5bf
Neil Ranada <neil.ranada@gmail.com>
2023-09-18 11:53:15 -0700

    Refactor test to be concise

--- MicrosoftTraining/mslearn-create-web-api-aspnet-core/ContosoPizzaApi/UnitTests/UnitTest1.cs
+++ MicrosoftTraining/mslearn-create-web-api-aspnet-core/ContosoPizzaApi/UnitTests/UnitTest1.cs
@@ -7,10 +7,6 @@ namespace UnitTests
     {
         [Fact]
         public void GetAll_Pizzas_Count_Should_Be_Two()
-        {
-            var Pizzas = PizzaService.GetAll();
-            var count = Pizzas.Count();
-            count.Should().Be(2);
-        }
+        => PizzaService.GetAll().Count().Should().Be(2);
     }
 }
