f56d962d496d9de6ca17c73278d989a119891661
Neil Ranada <neil.ranada@gmail.com>
2023-10-10 18:50:01 -0700

    Save progress with Microsoft Learn C# curriculum

---
+++ MicrosoftTraining/RandomDice/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Random dice = new Random();
+
+// Example of stateful (instance) method. Requires creating an instance of the class (ex. new Random())
+int roll = dice.Next(1, 7);
+
+// .WriteLine is a static method. Does not require creating an instance of a class
+Console.WriteLine(roll);
+
+// Will get a compilation error since a new object isn't created to access a stateful method
+// int result = Random.Next();
