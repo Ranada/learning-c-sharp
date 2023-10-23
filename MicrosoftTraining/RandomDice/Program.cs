1d1929c2ffcbdbce5529f7dc05b35f1aca2a4314
Neil Ranada <neil.ranada@gmail.com>
2023-10-23 15:43:03 -0700

    Review method overloads

--- MicrosoftTraining/RandomDice/Program.cs
+++ MicrosoftTraining/RandomDice/Program.cs
@@ -4,10 +4,21 @@
 Random dice = new Random();
 
 // Example of stateful (instance) method. Requires creating an instance of the class (ex. new Random())
-int roll = dice.Next(1, 7);
+// int roll = dice.Next(1, 7);
 
 // .WriteLine is a static method. Does not require creating an instance of a class
-Console.WriteLine(roll);
+// Console.WriteLine(roll);
 
 // Will get a compilation error since a new object isn't created to access a stateful method
 // int result = Random.Next();
+
+// Method overload demo
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+dice.Next(6);
