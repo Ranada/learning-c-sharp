3eeda4f9cf2c8c2c38b79d6fba1a7da571aedab0
Neil Ranada <neil.ranada@gmail.com>
2023-10-27 15:59:26 -0700

    Add conditional logic for win condition and enable game to replay

--- MicrosoftTraining/RandomDice/Program.cs
+++ MicrosoftTraining/RandomDice/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.Numerics;
+
 Random dice = new Random();
 
 // Example of stateful (instance) method. Requires creating an instance of the class (ex. new Random())
@@ -13,12 +15,42 @@ Random dice = new Random();
 // int result = Random.Next();
 
 // Method overload demo
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
 
-dice.Next(6);
+string? input;
+do
+{
+
+    int roll1 = dice.Next(1, 7);
+    int roll2 = dice.Next(1, 7);
+    int roll3 = dice.Next(1, 7);
+
+    int total = roll1 + roll2 + roll3;
+
+    if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus points to total!");
+        total += 2;
+    }
+
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! Winner, winner, chicken dinner! +6 bonus points to total!");
+        total += 6;
+    }
+
+    Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+    if (total >= 15)
+    {
+        Console.WriteLine("Congrats you win!");
+    }
+    else
+    {
+        Console.WriteLine("Womp womp. Sorry you loss.");
+    }
+    
+    Console.WriteLine("\nPress any key to continue. Or type 'exit' to end the program");
+    input = Console.ReadLine();
+
+} while (input == null || !input.ToLower().Equals("exit"));
