93af6b61dfe743ce016b76f6de4e9f9ec26a8aab
Neil Ranada <neil.ranada@gmail.com>
2023-10-27 18:42:22 -0700

    Add conditional logic to display various messages based on number of days until expiration

--- MicrosoftTraining/RenewSubscription/Program.cs
+++ MicrosoftTraining/RenewSubscription/Program.cs
@@ -1,6 +1,29 @@
 ﻿var random = new Random();
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
+string message;
 
-Console.WriteLine("Days until expiration: {0}", daysUntilExpiration);
-Console.WriteLine("Discount offer {0}", discountPercentage);
+if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    message = "within a day!";
+    ExpirationMessage();
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    message = $"in {daysUntilExpiration} days!";
+    ExpirationMessage();
+
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+
+void ExpirationMessage()
+{
+    Console.WriteLine("Your subscription expires {0}", message);
+    Console.WriteLine("Renew now and save {0}%!", discountPercentage);
+}
