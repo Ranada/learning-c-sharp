a48b40af76d921587f1379e0c53591574b68dd38
Neil Ranada <neil.ranada@gmail.com>
2023-10-28 23:55:33 -0700

    Implement insertion sort with two pointers

--- DataStructsAlgos/InsertionSort/Program.cs
+++ DataStructsAlgos/InsertionSort/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] InsertionSort(int[] nums)
+{
+    if (nums.Length < 2)
+    {
+        return nums;
+    }
+
+    for (int R = 1; R < nums.Length; R++)
+    {
+        int L = R - 1;
+        while (L >= 0 && nums[L] > nums[L + 1])
+        {
+            int temp = nums[L];
+            nums[L] = nums[L + 1];
+            nums[L + 1] = temp;
+            L -= 1;
+        }
+    }
+
+    return nums;
+}
+
+
+void PrintArray(int[] nums)
+{
+    foreach (int num in nums)
+    {
+        Console.Write($"{num} ");
+    }
+}
+
+var nums = new int[] {2,3,4,1,6}; 
+
+Console.Write("Before: ");
+PrintArray(nums);
+
+InsertionSort(nums);
+
+Console.Write("\nAfter: ");
+PrintArray(nums); // Expect 1 2 3 4 6
+
+// 1,2,3,4,6
+// L
+//       R
