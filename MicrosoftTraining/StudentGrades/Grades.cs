ef06653a3796d9ea56e8278ac3e5a06e1b88f36c
Neil Ranada <neil.ranada@gmail.com>
2023-10-31 11:57:17 -0400

    Initial commit for student grades console app project

---
+++ MicrosoftTraining/StudentGrades/Grades.cs
@@ -0,0 +1,31 @@
+namespace Students;
+
+public static class Grades
+{
+    // initialize variables - graded assignments 
+public static int currentAssignments = 5;
+
+static int sophia1 = 90;
+static int sophia2 = 86;
+static int sophia3 = 87;
+static int sophia4 = 98;
+static int sophia5 = 100;
+
+static int andrew1 = 92;
+static int andrew2 = 89;
+static int andrew3 = 81;
+static int andrew4 = 96;
+static int andrew5 = 90;
+
+static int emma1 = 90;
+static int emma2 = 85;
+static int emma3 = 87;
+static int emma4 = 98;
+static int emma5 = 68;
+
+static int logan1 = 90;
+static int logan2 = 95;
+static int logan3 = 87;
+static int logan4 = 88;
+static int logan5 = 96;
+}
